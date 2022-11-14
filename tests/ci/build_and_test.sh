73be7ae4fa7320cdaa2e720000c9f264eb90d29c
Neal Gompa <neal@gompa.dev>
2022-11-14 08:04:17 -0500

    Add initial GitHub Actions CI
    This setup is cribbed from what we use for PackageKit itself.

---
+++ tests/ci/build_and_test.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+if [ -d "build" ]; then
+  rm build -rf
+fi
+cmake -S . -B build -GNinja -DMAINTAINER=1 $@
+
+# Build, Test & Install
+cmake --build build
+## No tests yet
+#cd build && ctest && cd -
+
+DESTDIR=/tmp/install_root/ cmake --install build
