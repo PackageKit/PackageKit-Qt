9845fc13f249b71620941325d1277caf55d6b33d
Gleb Popov <6yearold@gmail.com>
2025-08-16 10:03:17 +0300

    Revert "offline: Ensure the enum is registered"
    This reverts commit 8a351053f16423033b292573cd83500a4fcd900b.

--- src/offline.cpp
+++ src/offline.cpp
@@ -26,7 +26,6 @@ using namespace PackageKit;
 Offline::Offline(QObject *parent) : QObject(parent)
   , d_ptr(new OfflinePrivate(this))
 {
-    qRegisterMetaType<Transaction::Role>();
     QDBusConnection::systemBus().connect(PK_NAME,
                                          PK_PATH,
                                          DBUS_PROPERTIES,
